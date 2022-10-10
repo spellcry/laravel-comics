2291bb5cd491023988bf9a385b21ab62a5f0e3f3
spellcry <sangiothebest@hotmail.com>
2022-10-10 18:13:49 +0200

    add comic page

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -4,7 +4,32 @@
 
 @section('content')
     @include('partials.hero')
-    <div class="container">
-        
+    <div class="comic">
+        <div class="container">
+            <div class="row">
+                <div class="col-70">
+                    <h2 class="title">{{ $comic['title'] }}</h2>
+                    <div class="status">
+                        <div class="price">
+                            U.S. Price: <span class="accent">{{ $comic['price'] }}</span>
+                        </div>
+                        <div class="availability">
+                            Available                    
+                        </div>
+                        <div class="check-availability">
+                            <span class="accent">Check Availability</span>
+                        </div>
+                    </div>
+                    <div class="description">
+                        <p>
+                            {{ $comic['description'] }}
+                        </p>
+                    </div>
+                </div>
+                <div class="col-30">
+                    <img class="adv" src="{{ asset('img/adv.jpg') }}" alt="">
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
